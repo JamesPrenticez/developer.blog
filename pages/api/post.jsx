328d0299d07dfe85ea33c07f48eda4c2ec746166
James Prentice <jamesprenticez@gmail.com>
2022-02-10 14:33:49 +1300

    Generate Slug

--- pages/api/post.jsx
+++
@@ -1,7 +0,0 @@
-import prisma from '../../lib/prisma';
-
-//Not used
-export default async function getPosts(req, res){
-  const posts = await prisma.post.findMany()
-  return res.status(200).json(posts)
-}
--- pages/post/create.jsx
+++ pages/post/create.jsx
@@ -4,12 +4,21 @@ import Header from '../../components/Header'
 
 function Create() {
   const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [slug, setSlug] = useState('');
   const [content, setContent] = useState('');
+
+  const generateSlug = (Text) => {
+    let slug = Text.toLowerCase()
+               .replace(/ /g, '-')
+               .replace(/[^\w-]+/g, '');
+    setSlug(slug)
+  }
 
   const submitData = async (e) => {
     e.preventDefault();
     try {
-      const body = { title, content };
+      const body = { title, content, description };
       await fetch('/api/post', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -27,7 +36,7 @@ function Create() {
       <div className='bg-green-50 flex justify-center items-center p-3 '>
         <form 
           onSubmit={submitData}
-          className="w-4/6"
+          className="w-4/6 space-y-1"
         >
           <h1 className='text-xl font-bold p-2'>Create New Draft</h1>
           <input
@@ -38,17 +47,43 @@ function Create() {
             type="text"
             value={title}
           />
-          <textarea
+          <input
+            autoFocus
+            className='w-full p-2 border rounded-sm'
+            onChange={(e) => setDescription(e.target.value)}
+            placeholder="Description"
+            type="text"
+            value={description}
+          />
+          <div className='flex w-full'>
+            <input
+              autoFocus
+              className='p-2 border rounded-sm inline flex-grow'
+              onChange={(e) => setSlug(e.target.value)}
+              placeholder="Slug"
+              type="text"
+              value={slug}
+            />
+            <input 
+              type='button'
+              onClick={() => generateSlug(title)}
+              className='inline flex-grow-0 px-4 bg-gray-300 hover:bg-gray-400 hover:cursor-pointer'
+              value="Generate Slug"
+            />
+          </div>
+
+          {/* <textarea
             cols={50}
             className='w-full p-2 border rounded-sm'
             onChange={(e) => setContent(e.target.value)}
             placeholder="Content"
             rows={8}
             value={content}
-          />
+          /> */}
+
+
           <div className='w-full flex space-x-2'>
             <button 
-              disabled={!content || !title}
               className="bg-red-600 p-2 text-white rounded-md hover:cursor-pointer hover:bg-red-700 ml-auto"
               type="button"
               value="Discard"
@@ -57,7 +92,7 @@ function Create() {
             </button>
              <button 
               disabled={!content || !title}
-              className="bg-green-600 p-2 text-white rounded-md hover:cursor-pointer hover:bg-green-700"
+              className="bg-green-600 p-2 text-white rounded-md hover:cursor-pointer hover:bg-green-700 disabled:cursor-not-allowed"
               type="submit"
               value="Create"
             >
@@ -66,8 +101,6 @@ function Create() {
           </div>
         </form>
       </div>
-
-
     </main>
   )
 }
